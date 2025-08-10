39b6a2b13cb01e3ef237fe48a2ba83defba36760
pratiksejpal-ps <pratiksejpal18@gmail.com>
2025-08-10 12:20:23 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandRed: '#e53935',
+        brandGreen: '#43a047'
+      }
+    }
+  },
+  plugins: []
+}
